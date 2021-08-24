f61298aab97a52300434d8950aa95a1335f25631
Jeremy Yoo <jyyoo824@gmail.com>
2021-08-24 16:30:59 +0100

    MVP completed

--- models/decorator.js
+++ models/decorator.js
@@ -1,6 +1,27 @@
 // Decorator Constructor and prototypes here
 const Decorator = function () {
-    
+    this.stock = [];
 }
+
+Decorator.prototype.addPaint = function (paint) {
+    this.stock.push(paint);
+}
+
+Decorator.prototype.calculateLiters = function () {
+    let value = 0;
+    for (paint of this.stock) {
+        value += paint.liters;
+    }
+    return value;
+}
+
+Decorator.prototype.calculateEnoughPaint = function (room) {
+    if (this.calculateLiters() > room.area) {
+        room.painted = true;
+        return true;
+    }
+    return false
+}
+
 
 module.exports = Decorator;
--- models/specs/decorator_spec.js
+++ models/specs/decorator_spec.js
@@ -1,37 +1,69 @@
 const assert = require('assert');
-// const Decorator = require('../decorator.js');
-// const PaintCan = require('../paint_can.js');
-// const Room = require('../room.js');
+const Decorator = require('../decorator.js');
+const PaintCan = require('../paint_can.js');
+const Room = require('../room.js');
 
 describe('Decorator', function () {
-
-
+  let decorator;
 
   beforeEach(function () {
- 
+    decorator = new Decorator()
   });
 
-  xit('should start with an empty paint stock', function () {
-    
+  it('should start with an empty paint stock', function () {
+    const actual = decorator.stock.length;
+    assert.strictEqual(actual, 0);
   });
 
-  xit('should be able to add a can of paint to paint stock', function () {
-  
+  it('should be able to add a can of paint to paint stock', function () {
+    const paint_1 = new PaintCan(10)
+    decorator.addPaint(paint_1);
+    const actual = decorator.stock.length;
+    assert.strictEqual(actual, 1);
   });
 
-  xit('should be able to calculate total litres paint it has in stock', function () {
-    
+  it('should be able to calculate total litres paint it has in stock', function () {
+    const paint_1 = new PaintCan(10)
+    decorator.addPaint(paint_1);
+    const paint_2 = new PaintCan(15)
+    decorator.addPaint(paint_2);
+
+    const actual = decorator.calculateLiters();
+    assert.strictEqual(actual, 25);
   });
 
-  xit('should be able to calculate that it has enough paint to paint a room', function () {
-    
+  it('should be able to calculate that it has enough paint to paint a room', function () {
+    const room_1 = new Room(20);
+    const paint_1 = new PaintCan(10)
+    decorator.addPaint(paint_1);
+    const paint_2 = new PaintCan(15)
+    decorator.addPaint(paint_2);
+
+    const actual = decorator.calculateEnoughPaint(room_1);
+    assert.strictEqual(actual, true);
   });
 
-  xit('should be able to calcuate that it doesnt have enough paint to paint a room', function () {
+  it('should be able to calcuate that it doesnt have enough paint to paint a room', function () {
+    const room_1 = new Room(30);
+    const paint_1 = new PaintCan(10)
+    decorator.addPaint(paint_1);
+    const paint_2 = new PaintCan(15)
+    decorator.addPaint(paint_2);
+
+    const actual = decorator.calculateEnoughPaint(room_1);
+    assert.strictEqual(actual, false);
   });
 
-  xit('should be able to paint room if has enough paint in stock', function () {
-   
+  it('should be able to paint room if has enough paint in stock', function () {
+    const room_1 = new Room(20);
+    const paint_1 = new PaintCan(10)
+    decorator.addPaint(paint_1);
+    const paint_2 = new PaintCan(15)
+    decorator.addPaint(paint_2);
+    decorator.calculateEnoughPaint(room_1);
+
+    const actual = room_1.painted;
+    assert.strictEqual(actual, true);
   });
 
   xit('should not paint room if it has not enough paint in stock', function () {
